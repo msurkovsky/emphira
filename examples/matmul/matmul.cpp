01f44ebacc50aff164d95013227a6fdfbb9f7fe7
Martin Surkovsky <martin.surkovsky@gmail.com>
2016-10-06 22:09:26 +0200

    RF: use two anonymous buffers except one for each submatrix

--- examples/matmul/matmul.cpp
+++ examples/matmul/matmul.cpp
@@ -32,7 +32,7 @@ int main (int argc, char **argv) {
 
   // distribution ==============================================================
   Matrix sub_a, sub_b, sub_c;
-  char *data_a, *data_b;
+  char *data_buff[2];
 
   if (rank == 0) {
     Matrix matrix_a(4, 4, {
@@ -56,9 +56,9 @@ int main (int argc, char **argv) {
       int j = p % n;
       int k = ceil((2 * n - 1 - i - j) % n);
 
+      // A(j, k)
       int sub_rows = matrix_a.rows() / n;
       int sub_cols = matrix_a.cols() / n;
-      // A(j, k)
       sub_a = matrix_a.extract_n(
         j * sub_rows,
         k * sub_rows,
@@ -66,15 +66,14 @@ int main (int argc, char **argv) {
         sub_cols
       );
 
-      data_a = new char[sub_a.get_byte_size()];
-      sub_a.serialize(data_a);
+      data_buff[0] = new char[sub_a.get_byte_size()];
+      sub_a.serialize(data_buff[0]);
       // TODO: may have a problem with standard send (blocking one)
-      MPI_Send(data_a, sub_a.get_byte_size(), MPI_BYTE, p, A, MPI_COMM_WORLD);
+      MPI_Send(data_buff[0], sub_a.get_byte_size(), MPI_BYTE, p, A, MPI_COMM_WORLD);
 
-
+      // B(k, i)
       sub_rows = matrix_b.rows() / n;
       sub_cols = matrix_b.cols() / n;
-      // B(k, i)
       sub_b = matrix_b.extract_n(
         k * sub_rows,
         i * sub_rows,
@@ -82,9 +81,9 @@ int main (int argc, char **argv) {
         sub_cols
       );
 
-      data_b = new char[sub_b.get_byte_size()];
-      sub_b.serialize(data_b);
-      MPI_Send(data_b, sub_b.get_byte_size(), MPI_BYTE, p, B, MPI_COMM_WORLD);
+      data_buff[1] = new char[sub_b.get_byte_size()];
+      sub_b.serialize(data_buff[1]);
+      MPI_Send(data_buff[1], sub_b.get_byte_size(), MPI_BYTE, p, B, MPI_COMM_WORLD);
     }
 
     // extract a matrix for zero
@@ -119,33 +118,33 @@ int main (int argc, char **argv) {
     int msg_size;
     MPI_Get_count(&status, MPI_BYTE, &msg_size);
 
-    data_a = new char[msg_size];
-    MPI_Recv(data_a, msg_size, MPI_BYTE, 0, A, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    sub_a.deserialize(data_a);
+    data_buff[0] = new char[msg_size];
+    MPI_Recv(data_buff[0], msg_size, MPI_BYTE, 0, A, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    sub_a.deserialize(data_buff[0]);
 
 
     MPI_Probe(0, B, MPI_COMM_WORLD, &status);
     MPI_Get_count(&status, MPI_BYTE, &msg_size);
 
-    data_b = new char[msg_size];
-    MPI_Recv(data_b, msg_size, MPI_BYTE, 0, B, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    sub_b.deserialize(data_b);
+    data_buff[1] = new char[msg_size];
+    MPI_Recv(data_buff[1], msg_size, MPI_BYTE, 0, B, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    sub_b.deserialize(data_buff[1]);
   }
   sub_c = Matrix(sub_a.rows(), sub_b.cols());
 
   // computation ===============================================================
   sub_c += sub_a * sub_b;
 
+  MPI_Request requests[2];
+  MPI_Status statuses[2];
   for (int i = 1; i < n; i++) { // from 1 because the first round is done
                                 // just after initial distribution
-    MPI_Request requests[2];
-    MPI_Status statuses[2];
 
     // the communication is done in two waves
     // first each process send and receive A matrix
     // and in the other one B matrix is send and received
-    sub_a.serialize(data_a);
-    MPI_Isend(data_a,
+    sub_a.serialize(data_buff[0]);
+    MPI_Isend(data_buff[0],
               sub_a.get_byte_size(),
               MPI_BYTE,
               next_a(rank, n),
@@ -153,19 +152,20 @@ int main (int argc, char **argv) {
               MPI_COMM_WORLD,
               &requests[0]);
 
-    MPI_Irecv(data_b, // use data_b for receive in order not to rewrite data_a buffer
+    MPI_Irecv(data_buff[1], // use the second buffer for receive in order
+                            // not to rewrite the first one
               sub_a.get_byte_size(),
               MPI_BYTE,
-              MPI_ANY_SOURCE,
+              MPI_ANY_SOURCE, // TODO: I know the originator process
               A,
               MPI_COMM_WORLD,
               &requests[1]);
 
     MPI_Waitall(2, requests, statuses);
-    sub_a.deserialize(data_b);
+    sub_a.deserialize(data_buff[1]);
 
-    sub_b.serialize(data_b);
-    MPI_Isend(data_b,
+    sub_b.serialize(data_buff[0]);
+    MPI_Isend(data_buff[0],
               sub_b.get_byte_size(),
               MPI_BYTE,
               next_b(rank, n),
@@ -173,16 +173,16 @@ int main (int argc, char **argv) {
               MPI_COMM_WORLD,
               &requests[0]);
 
-    MPI_Irecv(data_a,
+    MPI_Irecv(data_buff[1],
               sub_b.get_byte_size(),
               MPI_BYTE,
-              MPI_ANY_SOURCE,
+              MPI_ANY_SOURCE, // TODO:
               B,
               MPI_COMM_WORLD,
               &requests[1]);
 
     MPI_Waitall(2, requests, statuses);
-    sub_b.deserialize(data_a);
+    sub_b.deserialize(data_buff[1]);
 
     sub_c += sub_a * sub_b;
   }
@@ -197,14 +197,14 @@ int main (int argc, char **argv) {
     MPI_Status status;
     int i, j, p;
     for (int k = 1; k < size; k++) {
-      MPI_Recv(data_a, sub_c.get_byte_size(),
+      MPI_Recv(data_buff[0], sub_c.get_byte_size(),
                MPI_BYTE,
                MPI_ANY_SOURCE,
                C,
                MPI_COMM_WORLD,
                &status);
 
-      sub_c.deserialize(data_a);
+      sub_c.deserialize(data_buff[0]);
 
       p = status.MPI_SOURCE;
       i = p / n;
@@ -215,11 +215,9 @@ int main (int argc, char **argv) {
     std::cout << *matrix_c;
 
     delete matrix_c; // delete only on zero process, only there is initialized
-
-    // NOTE: this is also something I can check
   } else {
-    sub_c.serialize(data_a);
-    MPI_Send(data_a,
+    sub_c.serialize(data_buff[0]);
+    MPI_Send(data_buff[0],
              sub_c.get_byte_size(),
              MPI_BYTE,
              0,
@@ -227,8 +225,8 @@ int main (int argc, char **argv) {
              MPI_COMM_WORLD);
   }
 
-  delete[] data_a;
-  delete[] data_b;
+  delete[] data_buff[0];
+  delete[] data_buff[1];
 
   MPI_Finalize();
   return 0;
